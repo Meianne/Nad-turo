96585ae4a62e1bf5f8a5a5e35f5ae13ff0483269
Meianne <genevieve.prv@gmail.com>
2019-03-14 16:14:33 +0100

    first commit

---
+++ template/header.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Nad'Turopathie</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+  <link rel="icon" href="favicon.ico" />
+  <script src="https://www.google.com/recaptcha/api.js"></script>
+
+  <link href="https://fonts.googleapis.com/css?family=Exo+2" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100i,300,300i,400,400i,500,500i" rel="stylesheet">
+  <link rel="stylesheet" href="css/main.css">
+  </head>
+
+<body>
+  <header>
+    <div class="green">
+      <div class="red">
+        <img src="img/logov2-50.png" alt="logo">
+      </div>
+      <div>
+        <h1>Nadia Aïssaoui</h1>
+        <h2><span>- Naturopathe -</span></h2>
+      </div>
+    </div>
---
+++ template/nav.php
@@ -0,0 +1,77 @@
+<nav>
+  <div class="icon">
+    <img src="img/burger.png" alt="menu burger">
+  </div>
+
+  <div class="menu">
+    <label for="menu-mobile" class="bg-transparent text-white">Menu</label>
+    <input type="checkbox" role="button">
+    <ul class="nav nav-tabs my-5 mx-5">
+      <li>
+        <a class="btn text-white" href="index.php" role="button">Accueil</a>
+      </li>
+      <li>
+        <a class="btn text-white" href="apropos.php" role="button">A propos</a>
+      </li>
+      <li><a class="btn text-white" href="prestations.php">Prestations</a>
+        <ul>
+          <li>
+            <a class="btn bg-transparent text-white dropdown-item" href="naturopathie.php">Naturopathie</a>
+          </li>
+          <li>
+            <a class="btn bg-transparent text-white dropdown-item" href="soin-energy.php">Soins énergétiques</a>
+          </li>
+          <li>
+            <a class="btn bg-transparent text-white dropdown-item" href="neurosciences.php">Neurosciences</a>
+          </li>
+        </ul>
+      </li>
+      <!-- <li>
+        <a class="btn text-white" href="temoignages.php" role="button">Témoignages</a>
+      </li> -->
+      <li>
+        <a class="btn bg-transparent text-white" href="https://www.timify.com/fr-fr/profile/nadia-aissaoui" target="_blank" role="button">
+          Réservation en ligne
+        </a>
+      </li>
+      <li>
+        <a class="btn text-white" href="contact.php" role="button">Contact</a>
+      </li>
+    </ul>
+  </div>
+
+  <div class="pcMenu">
+    <ul class="nav nav-tabs my-5 mx-5">
+      <li>
+        <a class="btn text-white" href="index.php" role="button">Accueil</a>
+      </li>
+      <li>
+        <a class="btn text-white" href="apropos.php" role="button">A propos</a>
+      </li>
+      <li><a class="btn text-white" href="prestations.php">Prestations</a>
+        <ul class="submenu">
+          <li>
+            <a class="btn bg-transparent text-white dropdown-item" href="naturopathie.php">Naturopathie</a>
+          </li>
+          <li>
+            <a class="btn bg-transparent text-white dropdown-item" href="soin-energy.php">Soins énergétiques</a>
+          </li>
+          <li>
+            <a class="btn bg-transparent text-white dropdown-item" href="neurosciences.php">Neurosciences</a>
+          </li>
+        </ul>
+      </li>
+      <!-- <li>
+        <a class="btn text-white" href="temoignages.php" role="button">Témoignages</a>
+      </li> -->
+      <li>
+        <a class="btn bg-transparent text-white" href="https://www.timify.com/fr-fr/profile/nadia-aissaoui" target="_blank" role="button">
+          Réservation en ligne
+        </a>
+      </li>
+      <li>
+        <a class="btn text-white" href="contact.php" role="button">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
